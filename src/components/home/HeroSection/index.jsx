584cc1a2fbbf7edf29426f99491a268d3e48aa76
param-ninthu <ninthu1999@gmail.com>
2022-09-05 12:39:25 +0530

    change folder structure

---
+++ src/components/home/HeroSection/index.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { ArrowForward, ArrowRight, HeroBg, HeroButtonWrapper, HeroContainer, HeroContent, HeroH1, HeroP, VideoBg } from './HeroElements';
+import Video from './../../videos/wedding.mp4';
+import { Button } from '../../ButtonElements';
+
+
+const HeroSection = () => {
+  const [hover,setHover] =useState(false);
+
+  const onHover = ()=>{
+    setHover(!hover)
+  }
+  return (
+    <>
+        <HeroContainer>
+          <HeroBg>
+            <VideoBg src={Video} autoPlay loop muted type='video/mp4'/>
+          </HeroBg> 
+          <HeroContent>
+            <HeroH1> Dream your wedding with knot  </HeroH1>
+            <HeroP>
+            A great marriage isn’t something that just happens- it’s something that must be created.
+            </HeroP>
+            <HeroButtonWrapper>
+              <Button to='signup' onMouseEnter={onHover} onMouseLeave={onHover} primary='true' dark='true' > Get started {hover ? <ArrowForward/> : <ArrowRight/>}  </Button>
+            </HeroButtonWrapper>
+          </HeroContent>
+        </HeroContainer>
+    </>
+  )
+}
+
+export default HeroSection;
