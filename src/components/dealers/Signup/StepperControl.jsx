86425dc3cbfdaa5b99cb11019d02cc9df0fd44eb
param-ninthu <ninthu1999@gmail.com>
2022-09-05 23:19:37 +0530

    changes

--- src/components/dealers/Signup/StepperControl.jsx
+++ src/components/dealers/Signup/StepperControl.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 
 
@@ -23,30 +22,4 @@ const StepperControl = ({handleClick,currentStep,steps,submitData,otpGenerate}) 
   )
 }
 
-=======
-import React from 'react'
-
-
-const StepperControl = ({handleClick,currentStep,steps,submitData,otpGenerate}) => {
-
-
-  
-  return (
-    <div className='container flex justify-around mt-4 mb-8'>
-        <button
-          onClick={() => handleClick()}
-          className={`bg-white text-slate-400 uppercase py-2 px-4 rounded-xl font-semibold cursor-pointer border-2 border-slate-300 hover:bg-slate-700 hover:text-white transition duration-200 ease-in-out ${currentStep == 1 ? "opacity-50 cursor-not-allowed":""}`}>
-            Back
-        </button>
-
-        <button 
-          onClick={() =>  currentStep === steps.length -1 ?    submitData() : currentStep === steps.length -2 ? otpGenerate() : handleClick("next")}
-          className='bg-green-500 text-white uppercase py-2 px-4 rounded-xl font-semibold cursor-pointer border-2 border-slate-300 hover:bg-slate-700 hover:text-white transition duration-200 ease-in-out'>
-          {currentStep === steps.length -1  ? "Confirm" : "Next" }
-        </button>
-    </div>
-  )
-}
-
->>>>>>> 29f386f35d00451c03c2b5d2aee8a2625e1c3fa7
 export default StepperControl;
--- src/components/dealers/Signup/otp.jsx
+++ src/components/dealers/Signup/otp.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const generateOTP = () => {
           
     // Declare a string variable 
@@ -14,21 +13,4 @@ const generateOTP = () => {
     return OTP;
 }
 
-=======
-const generateOTP = () => {
-          
-    // Declare a string variable 
-    // which stores all string
-    var string = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    let OTP = '';
-      
-    // Find the length of string
-    var len = string.length;
-    for (let i = 0; i < 6; i++ ) {
-        OTP += string[Math.floor(Math.random() * len)];
-    }
-    return OTP;
-}
-
->>>>>>> 29f386f35d00451c03c2b5d2aee8a2625e1c3fa7
 module.exports = generateOTP;
