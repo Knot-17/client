ba92a94f9d4a60f455e1ddd10e33eb68de0eae7f
Studawatta <89610546+Studawatta@users.noreply.github.com>
2022-10-23 05:27:12 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import UserScreen from './screens/userView';
 import Profile from './components/clients/Profile/Profile';
 import SignUpPageDealers from './screens/signupdealers';
 import Dashboard from './components/admin/Dashboard/Dashboard';
-
+import ClientDash from './screens/ClientDash';
 
 import './assets/boxicons-2.0.7/css/boxicons.min.css'
 
@@ -17,6 +17,7 @@ import Routes from './components/clients/Profile/Routes/Routes';
 import Routes_Dealers from './components/dealers/Routes/Routes_Dealers';
 import Routes_Common from './components/common/Routes/Routes_Common';
 import Routes_Admin from './components/admin/Dashboard/Routes/Routes_Admin';
+import Routes_Clients from './components/clients/ClientDashboard/Routes/Route_Clients';
 function App() {
   return (
     <Router>
@@ -28,10 +29,12 @@ function App() {
           <Route path='/user' component={UserScreen} exact />
           <Route path='/user/profile' component={Profile} exact />
           <Route path='/admin/profile' component={Dashboard} exact />
+          <Route path='/clientdash' component={ClientDash} exact />
           {Routes.map(props => <Route exact {...props} />)}
           {Routes_Dealers.map(props => <Route exact {...props} />)}
           {Routes_Common.map(props => <Route exact {...props} />)}
           {Routes_Admin.map(props => <Route exact {...props} />)}
+          {Routes_Clients.map(props => <Route exact {...props} />)}
         </Switch>
     </Router>
   );
